deb5fb39e62e1587aa8455a5b9090e271869ed65
joyle1 <12914448+joyle1@users.noreply.github.com>
2021-06-11 08:56:19 +0200

    TimeUnit.SECONDS.sleep(10);

--- src/TestingSelenium.java
+++ src/TestingSelenium.java
@@ -1,9 +1,10 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import java.util.concurrent.TimeUnit;
 
 public class TestingSelenium {
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
 
         //setting the driver executable
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\klamnlo\\Downloads\\geckodriver-v0.29.1-win64\\geckodriver.exe");
@@ -15,6 +16,11 @@ public class TestingSelenium {
         WebElement button=driver.findElement(By.xpath("//input[@name='btnLogin']"));
         button.click();
 
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        TimeUnit.SECONDS.sleep(10);
+
+        //driver.wait(10);
+        //Thread.sleep(ThreadLocalRandom.current().nextInt(1000, 5000));
         //closing the browser
         driver.close();
 
